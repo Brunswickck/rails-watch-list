0d744a039cd068da100d3b5a8ad44c046d06d697
tom burns <tomburns.gj@gmail.com>
2023-03-10 01:25:06 +0000

    finished links & completed basic structure of site through css

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,10 @@
 class BookmarksController < ApplicationController
 
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
@@ -7,7 +12,7 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
-        redirect_to list_path(@list), status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 10 }
-  validates :rating, numericality: { only_integer: true, in: (1..5)}
+  validates :rating, presence: :true, numericality: { only_integer: true, in: (1..5)}
 end
