84b103bf965b1f692641534b1de33cb7485b82c1
tom burns <tomburns.gj@gmail.com>
2023-03-10 17:37:34 +0100

    quick commit for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the  DSL in https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
 
   resources :lists, only: [:index, :show, :new, :create] do
